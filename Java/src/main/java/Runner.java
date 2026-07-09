ee90f72e39501bf89d14ddcc12313cbb98b05dc8
Danik <momentssd@gmail.com>
2026-07-09 21:09:32 +0300

    Update Runner.java
    Изменил название функций, оптимизировал код. Результат теперь в переменной

--- Java/src/main/java/Runner.java
+++ Java/src/main/java/Runner.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
-    static List<Plane> planes = Arrays.asList(
+    private static final List<Plane> planes = Arrays.asList(
             new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
             new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
             new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
@@ -26,15 +26,16 @@ public class Runner {
 
     public static void main(String[] args) {
         Airport airport = new Airport(planes);
+        
         Airport militaryAirport = new Airport(airport.getMilitaryPlanes());
-        Airport passengerAirport = new Airport(airport.getPasPl());
-        System.out.println("Military airport sorted by max distance: " + militaryAirport
-                .sortByMaxDistance()
-                .toString());
-        System.out.println("Passenger airport sorted by max speed: " + passengerAirport
-                .sortByMaxSpeed()
-                .toString());
-
-        System.out.println("Plane with max passenger capacity: " + passengerAirport.getPassengerPlaneWithMaxPassengersCapacity());
+        militaryAirport.sortByMaxDistance();
+        System.out.println("Military airport sorted by max distance: " + militaryAirport);
+        
+        Airport passengerAirport = new Airport(airport.getPassengerPlanes());
+        passengerAirport.sortByMaxSpeed();
+        System.out.println("Passenger airport sorted by max speed: " + passengerAirport);
+        
+        PassengerPlane maxCapacityPlane = passengerAirport.getPassengerPlaneWithMaxPassengersCapacity();
+        System.out.println("Plane with max passenger capacity: " + maxCapacityPlane);
     }
 }
